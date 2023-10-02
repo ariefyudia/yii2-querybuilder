583becfbba0097840a79ebed06571d65925e7de0
ariefyudia <ariefyudia@gmail.com>
2023-10-03 00:01:40 +0700

    Update Operator.php

--- Operator.php
+++ Operator.php
@@ -12,7 +12,7 @@ use yii\base\BaseObject;
  * @see http://mistic100.github.io/jQuery-QueryBuilder/#operators
  * @author Leandro Gehlen <leandrogehlen@gmail.com>
  */
-class Operator extends Object implements Optionable
+class Operator extends BaseObject implements Optionable
 {
     use OptionTrait;
 
